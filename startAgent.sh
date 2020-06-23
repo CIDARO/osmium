efec648430878a04ac5c8af3e5db37e86bee245e
PaoloRollo <paolorollo1997@hotmail.it>
2020-06-23 10:22:49 +0200

    added absolute path.

--- startAgent.sh
+++ startAgent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(crontab -l 2>/dev/null; echo "0 0 * * * node backup-agent/index.js") | crontab -
+(crontab -l 2>/dev/null; echo "0 0 * * * node /usr/local/osmium/backup-agent/index.js") | crontab -
